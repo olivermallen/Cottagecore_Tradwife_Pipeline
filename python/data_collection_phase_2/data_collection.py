2b730b2b5f749e1cb96e2481183cc511434959b1
Mel Allen <allen.ol@northeastern.edu>
2024-03-26 14:52:45 -0400

    replacing data collection files, prepping to remove keys

---
+++ python/data_collection_phase_2/data_collection.py
@@ -0,0 +1,42 @@
+import pytumblr
+import time
+import pickle
+from datetime import datetime
+
+
+keysfile = open("/home/mel/Desktop/repos/cottagecore_tradfem_pipeline/keys.txt", "r")
+keys = keysfile.readlines()
+
+
+client = pytumblr.TumblrRestClient(
+    keys[0].strip(),
+    keys[1].strip(),
+    keys[0].strip(),
+    keys[1].strip(),
+)
+# time_pointer starts at current time and goes back by grabbing the epoch of the last post collected
+
+# alternate tags
+
+time_pointer = int(time.time())
+i = 0
+
+for x in range(0, 9):  # try 4 times
+    try:
+        print("collecting posts at time " + str(datetime.fromtimestamp(time_pointer)))
+        posts = client.tagged(tag="cottagecore", before=time_pointer)
+        time_pointer = posts[19]["timestamp"]
+        with open(
+            "pickle_files" + str(time_pointer) + "_" + str(i) + ".pkl", "wb"
+        ) as f:
+            pickle.dump(posts, f, protocol=pickle.HIGHEST_PROTOCOL)
+        str_error = None
+        i = i + 1
+    except Exception as str_error:
+        print(str_error)
+        pass
+
+    if str_error:
+        time.sleep(
+            3690
+        )  # wait for one hour and a few seconds before trying to fetch the data again
